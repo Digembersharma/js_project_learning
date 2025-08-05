00a745feb893860c8356317d72eb778405e0942a
Digembersharma <digember.gla_cs22@gla.ac.in>
2025-08-05 17:15:49 +0530

    there is changement on js

--- tic-tak_game/script.js
+++ tic-tak_game/script.js
@@ -1,7 +1,8 @@
  let boxes = document.querySelectorAll('.box');
 
 
- let player0= true; // 
+ let player0= true; //
+ let resetBtn = document.querySelectorAll('reset-btn') 
 
 
  const wining_consdition =[
@@ -30,8 +31,34 @@
         // but there is a problem when we click it twice it will chang so we have to disable the bos to this we use
         box.disabled=true
         // harr box ka liy allag alag chalage disabled
+        checkwinner()
     })
     
-  });
+  })
+
+  const checkwinner=()=>{
+    for(let passvalue of wining_consdition){
+        
+        let passvalue1= boxes[passvalue[0]].innerText
+        
+        let passvalue2= boxes[passvalue[1]].innerText
+         
+        let passvalue3= boxes[passvalue[2]].innerText
+
+         console.log("row element we take"+ " " + passvalue +" " + passvalue1)
+        
+
+        if(passvalue1 !="" && passvalue2!="" && passvalue3!=""){
+            if(passvalue1==passvalue2 && passvalue2==passvalue3){
+                console.log('winner is found'+passvalue1);
+            }
+        }
+
+         
+         
+    }
+   
+     
+  }
 
  
